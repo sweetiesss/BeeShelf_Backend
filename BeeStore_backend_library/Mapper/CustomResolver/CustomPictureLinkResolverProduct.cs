cabc48c024d631790841744e24e595894109d5da
nhmnhj <mntaldisableme@gmail.com>
2024-09-30 08:45:58 +0700

    minor fix for DTO

---
+++ BeeStore_backend_library/Mapper/CustomResolver/CustomPictureLinkResolverProduct.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using BeeStore_Repository.Data;
+using BeeStore_Repository.DTO.ProductDTOs;
+using BeeStore_Repository.DTO.UserDTOs;
+using BeeStore_Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeStore_Repository.Mapper.CustomResolver
+{
+    public class CustomPictureLinkResolverProduct : IValueResolver<Product, ProductListDTO, string?>
+    {
+        private readonly BeeStoreDbContext _context;
+
+        public CustomPictureLinkResolverProduct(BeeStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public string Resolve(Product source, ProductListDTO destination, string destMember, ResolutionContext context)
+        {
+            var picture = _context.Pictures.FirstOrDefault(r => r.Id == source.PictureId);
+            return picture != null ? picture.PictureLink : null;
+        }
+    }
+}
---
+++ BeeStore_backend_library/Mapper/CustomResolver/CustomWarehouseNameResolver.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using BeeStore_Repository.Data;
+using BeeStore_Repository.DTO.InventoryDTOs;
+using BeeStore_Repository.DTO.UserDTOs;
+using BeeStore_Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeStore_Repository.Mapper.CustomResolver
+{
+    public class CustomWarehouseNameResolver : IValueResolver<Inventory, InventoryListDTO, string>
+    {
+        private readonly BeeStoreDbContext _context;
+
+        public CustomWarehouseNameResolver(BeeStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public string Resolve(Inventory source, InventoryListDTO destination, string destMember, ResolutionContext context)
+        {
+            var warehouse = _context.Warehouses.FirstOrDefault(r => r.Id == source.WarehouseId);
+            return warehouse != null ? warehouse.Name : null;
+        }
+    }
+}
